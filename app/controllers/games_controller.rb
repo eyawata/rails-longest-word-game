19d2e5f8d54b807569961676532ceff674ce6ce6
Eri Yawata <yawata.eri@gmail.com>
2024-08-05 17:01:23 +0900

    adding score functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,8 @@ class GamesController < ApplicationController
   def new
     letter = ('a'..'z').to_a
     @letters = Array.new(10) { letter.sample.upcase }
+    session[:letters] = @letters
+    @score = @score
   end
 
   def check_letters(letters, word_arr)
@@ -19,19 +21,21 @@ class GamesController < ApplicationController
   end
 
   def score
-    @letters_arr = params[:letters].split(" ")
+    @letters_arr = session[:letters]
     @word_arr = params[:word].upcase.split("")
+    @score = session[:score]
     # The word can’t be built out of the original grid ❌
     # The word is valid according to the grid, but is not a valid English word ❌
     # The word is valid according to the grid and is an English word ✅
     if @word_arr.all? { |letter| @letters_arr.include?(letter) }
-      @score = "#{params[:word]} was built from array!!!"
+      @message = "#{params[:word]} was built from array!!!"
       result = URI.open("https://dictionary.lewagon.com/#{params[:word]}").read
       result = JSON.parse(result)
       if result["found"] == true && check_letters(@letters_arr, @word_arr)
-        @score = "#{params[:word]} is from the array and is a valid word!!!"
+        @message = "#{params[:word]} is from the array and is a valid word!!!"
+        @score += params[:word].length
       else
-        @score = "#{params[:word]} is from the array and is NOT a valid word!!!"
+        @message = "#{params[:word]} is from the array and is NOT a valid word!!!"
       end
     else
       @score = "`#{params[:word]}` was not built from array!!!"
